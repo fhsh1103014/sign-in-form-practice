5788c78ae16cdefabe4bc8a271640d982a78292b
1103014 <11030144@fhsh.tp.edu.tw>
2022-05-06 12:00:15 +0800

    基礎版型完成

--- src/main.js
+++ src/main.js
@@ -4,4 +4,23 @@ $(document).ready(function () {
   $("#sign-in-form").submit(function (e) {
     e.preventDefault();
   });
+
+  //TODO: 讓帳號密碼輸入時，版型維持
+  $("#account").blur(function (e) {
+    if ($(e.target).val() !== "") {
+      $("#addr-hint-text").addClass("-translate-y-5 text-xs text-sky-500");
+    } else {
+      $("#addr-hint-text").removeClass("-translate-y-5 text-xs text-sky-500");
+    }
+  });
+
+  $("#password").blur(function (e) {
+    if ($(e.target).val() !== "") {
+      $("#password-hint-text").addClass("-translate-y-5 text-xs text-sky-500");
+    } else {
+      $("#password-hint-text").removeClass(
+        "-translate-y-5 text-xs text-sky-500"
+      );
+    }
+  });
 });
